86e2dedb11fad5be00a69b3b37e087b56fa53079
shalantor <gkaraolanis@uth.gr>
2016-10-05 18:26:34 +0300

    Add simple draw method in run to test activity starting with intent

--- app/src/main/java/com/example/shalantor/connect4/GameActivity.java
+++ app/src/main/java/com/example/shalantor/connect4/GameActivity.java
@@ -2,6 +2,7 @@ package com.example.shalantor.connect4;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 
 public class GameActivity extends Activity {
 
@@ -14,5 +15,35 @@ public class GameActivity extends Activity {
         gamePlayView = new GamePlayActivity(this);
 
         setContentView(gamePlayView);
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        gamePlayView.pause();
+        finish();
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        gamePlayView.resume();
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        gamePlayView.pause();
+    }
+
+    /*On key down terminate application*/
+    @Override
+    public boolean onKeyDown(int keycode, KeyEvent event){
+        if(keycode == KeyEvent.KEYCODE_BACK){
+            gamePlayView.pause();
+            finish();
+            return true;
+        }
+        return false;
     }
 }
--- app/src/main/java/com/example/shalantor/connect4/GamePlayActivity.java
+++ app/src/main/java/com/example/shalantor/connect4/GamePlayActivity.java
@@ -1,20 +1,47 @@
 package com.example.shalantor.connect4;
 
 
+import android.app.Activity;
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Point;
+import android.util.Log;
+import android.view.Display;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 public class GamePlayActivity extends SurfaceView implements Runnable{
 
     private Thread thread = null;
-    SurfaceHolder holder;
+    private SurfaceHolder holder;
     volatile boolean playingConnect4;
-    Paint paint;
+    private Paint paint;
+    private Canvas canvas;
+
+    /*fps*/
+    private long lastFrameTime;
+
+    /*Screen dimensions info*/
+    private int screenHeight;
+    private int screenWidth;
+
+    /*Associated activity*/
+    private Activity associatedActivity;
 
     public GamePlayActivity(Context context){
         super(context);
+        holder = getHolder();
+        paint = new Paint();
+        associatedActivity = (Activity) context;
+
+        /*Get screen dimensions*/
+        Display display = associatedActivity.getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        screenHeight = size.y;
+        screenWidth = size.x;
 
     }
 
@@ -36,13 +63,48 @@ public class GamePlayActivity extends SurfaceView implements Runnable{
 
     /*Method to draw the game fields and the menu on screen*/
     public void drawScreen(){
-
+        if(holder.getSurface().isValid()){
+            canvas = holder.lockCanvas();
+            paint.setColor(Color.argb(255,255,255,255));
+            canvas.drawColor(Color.WHITE);
+            holder.unlockCanvasAndPost(canvas);
+        }
     }
 
 
-    /*Method to control the fps*/
+    /*Method to control the refresh rate of screen*/
     public void controlFPS(){
-        
+
+        long timeThisFrame = System.currentTimeMillis() - lastFrameTime;
+        long timeToSleep = 50 - timeThisFrame;
+
+        if(timeToSleep > 0){
+            try{
+                thread.sleep(timeToSleep);
+            }catch (InterruptedException ex){
+                Log.d("THREAD_SLEEP","Interrupted exception occured");
+            }
+        }
+
+        lastFrameTime = System.currentTimeMillis();
+
+    }
+
+    /*To pause animation*/
+    public void pause(){
+        playingConnect4 = false;
+        try{
+            thread.join();
+        }catch (InterruptedException ex){
+            Log.d("THREAD_JOIN","Interrupted exception occured");
+        }
+    }
+
+    /*To continue animation from a pause*/
+    public void resume(){
+        playingConnect4 = true;
+        thread = new Thread(this);
+        thread.start();
     }
 
 }
