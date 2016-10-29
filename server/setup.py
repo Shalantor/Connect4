4fdf160e3fb448ed317ed79086652e91c47db247
shalantor <gkaraolanis@uth.gr>
2016-10-29 15:34:13 +0300

    Add setup file for server

---
+++ server/setup.py
@@ -0,0 +1,26 @@
+import sqlite3
+
+#create database
+connection = sqlite3.connect('connect4.db')
+cursor = connection.cursor()
+
+#Create a table for users logging in from connect4 game
+cursor.execute('''CREATE TABLE IF NOT EXISTS Users (username text,
+                                salt text,
+                                password text,
+                                wins integer,
+                                losses integer,
+                                elo integer,
+                                PRIMARY KEY(username))''')
+
+#Create a table for users logging in from facebook
+cursor.execute('''CREATE TABLE IF NOT EXISTS UsersFacebook (facebookid text,
+                                name text,
+                                wins integer,
+                                losses integer,
+                                elo integer,
+                                PRIMARY KEY(facebookid))''')
+
+connection.commit()
+
+cursor.close()
