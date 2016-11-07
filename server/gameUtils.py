8fa1ba3e022d55d0516f29d0d42c68bc0fd671bb
shalantor <gkaraolanis@uth.gr>
2016-11-07 15:34:21 +0200

    rewrite check function, now works

--- server/gameUtils.py
+++ server/gameUtils.py
@@ -8,18 +8,18 @@ def hasWon(board,chip):
             if board[row][col] == chip and board[row][col+1] == chip and board[row][col+2] == chip and board[row][col+3] == chip:
                 return True
     #check vertical
-    for col in range(0,6):
-        for row in range(0,4):
-            if board[row][col] == chip and board[row][col+1] == chip and board[row][col+2] == chip and board[row][col+3] == chip:
+    for col in range(0,7):
+        for row in range(0,3):
+            if board[row][col] == chip and board[row+1][col] == chip and board[row+2][col] == chip and board[row+3][col] == chip:
                 return True
     #check / diagonal
-    for row in range(0,4):
-        for col in range(3, 6):
-            if board[row][col] == chip and board[row+1][col-1] == chip and board[row+2][col-2] == chip and board[row+3][col-3] == chip:
+    for row in range(3,6):
+        for col in range(0, 4):
+            if board[row][col] == chip and board[row-1][col+1] == chip and board[row-2][col+2] == chip and board[row-3][col+3] == chip:
                 return True
     #check \ diagonal
-    for row in range(0,4):
-        for col in range(0,3):
+    for row in range(0,3):
+        for col in range(0,4):
             if board[row][col] == chip and board[row+1][col+1] == chip and board[row+2][col+2] == chip and board[row+3][col+3] == chip:
                 return True
     return False
