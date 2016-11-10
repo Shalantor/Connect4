bcd4f831c8dfc52154b4d9e1a4d1b9c422d8cc9d
shalantor <gkaraolanis@uth.gr>
2016-11-10 22:55:47 +0200

    Font sizes now change dynamically based on screen dimensions

--- app/src/main/java/com/example/shalantor/connect4/LoginActivity.java
+++ app/src/main/java/com/example/shalantor/connect4/LoginActivity.java
@@ -1,13 +1,49 @@
 package com.example.shalantor.connect4;
 
+import android.graphics.Point;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Display;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    public static final int SCREEN_TO_TEXT_SIZE_RATIO = 20;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        adjustTextSize();
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+    }
+
+    /*Method to adjust text size*/
+    private void adjustTextSize(){
+
+        /*Get references to buttons and editText*/
+        Button continueButton = (Button) findViewById(R.id.continue_button);
+        Button loginButton = (Button) findViewById(R.id.login_button);
+        Button registerButton = (Button) findViewById(R.id.register_button);
+        Button fbButton = (Button) findViewById(R.id.login_fb_button);
+        final EditText address = (EditText) findViewById(R.id.ip_address);
+
+        /*Get screen dimensions*/
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        int height = size.y;
+
+        /*set dimensions of components according to screen size*/
+        continueButton.setTextSize(height / SCREEN_TO_TEXT_SIZE_RATIO);
+        loginButton.setTextSize(height / SCREEN_TO_TEXT_SIZE_RATIO);
+        registerButton.setTextSize(height / SCREEN_TO_TEXT_SIZE_RATIO);
+        fbButton.setTextSize(height / SCREEN_TO_TEXT_SIZE_RATIO);
+        address.setTextSize(height / SCREEN_TO_TEXT_SIZE_RATIO);
+        address.setSelected(false);
+        address.clearFocus();
+
     }
 }
