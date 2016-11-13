00de6ab3a2bc898c6ca9c8a8279351b7e6c4b1f8
shalantor <gkaraolanis@uth.gr>
2016-11-13 22:24:52 +0200

    Add email verification method to register button

--- app/src/main/java/com/example/shalantor/connect4/RegisterFragment.java
+++ app/src/main/java/com/example/shalantor/connect4/RegisterFragment.java
@@ -11,10 +11,16 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class RegisterFragment extends Fragment{
 
     public static final int SCREEN_TO_TEXT_SIZE_RATIO = 20;
+    public static final Pattern VALID_EMAIL_ADDRESS_REGEX =
+            Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -36,10 +42,11 @@ public class RegisterFragment extends Fragment{
 
         /*Get references to components*/
         EditText usernamePrompt = (EditText) activity.findViewById(R.id.register_username);
-        EditText emailPrompt = (EditText) activity.findViewById(R.id.register_email);
+        final EditText emailPrompt = (EditText) activity.findViewById(R.id.register_email);
         EditText passwordPrompt = (EditText) activity.findViewById(R.id.register_password);
         EditText passwordVerify = (EditText) activity.findViewById(R.id.register_password_verify);
         Button registerButton = (Button) activity.findViewById(R.id.register_button_final);
+        final TextView textView = (TextView) activity.findViewById(R.id.error_messages_register);
 
         /*Set text sizes*/
         usernamePrompt.setTextSize(displayHeight / SCREEN_TO_TEXT_SIZE_RATIO);
@@ -47,6 +54,24 @@ public class RegisterFragment extends Fragment{
         passwordPrompt.setTextSize(displayHeight / SCREEN_TO_TEXT_SIZE_RATIO);
         passwordVerify.setTextSize(displayHeight / SCREEN_TO_TEXT_SIZE_RATIO);
         registerButton.setTextSize(displayHeight / SCREEN_TO_TEXT_SIZE_RATIO);
+
+        /*Add listener to register button*/
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                /*Check if email is in right format */
+                String emailInput = emailPrompt.getText().toString();
+                Matcher matcher = VALID_EMAIL_ADDRESS_REGEX.matcher(emailInput);
+                if (!matcher.find()){
+                    String errorMessage = "Please enter a valid email address format";
+                    textView.setText(errorMessage, TextView.BufferType.NORMAL);
+                }
+
+                /*Check if password prompts have the same password*/
+
+            }
+        });
 
     }
 }
