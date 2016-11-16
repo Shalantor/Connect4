5d7432f6233aa0e9c7c468326021f50c18fbda52
shalantor <gkaraolanis@uth.gr>
2016-11-16 21:06:10 +0200

    Add code to get user name and id, still doesnt get email

--- app/src/main/java/com/example/shalantor/connect4/AccountFragment.java
+++ app/src/main/java/com/example/shalantor/connect4/AccountFragment.java
@@ -7,6 +7,7 @@ import android.content.pm.ActivityInfo;
 import android.graphics.Point;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,15 +16,22 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphRequestAsyncTask;
+import com.facebook.GraphResponse;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
+import org.json.JSONObject;
+
 import java.util.Arrays;
+import java.util.concurrent.ExecutionException;
 
 public class AccountFragment extends Fragment{
 
@@ -31,6 +39,9 @@ public class AccountFragment extends Fragment{
     public static final String USER_TYPE = "USER_TYPE";
     private CallbackManager callbackManager;
     private Activity activity;
+    private String facebookId;
+    private String username;
+    private String email;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -38,6 +49,7 @@ public class AccountFragment extends Fragment{
         // Inflate the layout for this fragment
         activity = getActivity();
         FacebookSdk.sdkInitialize(activity.getApplicationContext());
+        LoginManager.getInstance().logOut();
         callbackManager = CallbackManager.Factory.create();
 
         getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
@@ -59,7 +71,7 @@ public class AccountFragment extends Fragment{
         final Activity activity = getActivity();
 
         /*Get references to buttons and editText*/
-        Button loginButton = (Button) activity.findViewById(R.id.login_button);
+        final Button loginButton = (Button) activity.findViewById(R.id.login_button);
         Button registerButton = (Button) activity.findViewById(R.id.register_button);
         LoginButton fbButton = (LoginButton) activity.findViewById(R.id.login_fb_button);
         EditText address = (EditText) activity.findViewById(R.id.ip_address);
@@ -88,21 +100,43 @@ public class AccountFragment extends Fragment{
                     @Override
                     public void onSuccess(LoginResult loginResult) {
                         TextView textView = (TextView) activity.findViewById(R.id.error_messages);
-                        String message = "LOGIN OK";
+                        AccessToken token = loginResult.getAccessToken();
+
+                        /*Asynchronous request task to get user data*/
+                        GraphRequestAsyncTask request = GraphRequest.newMeRequest(token,new GraphRequest.GraphJSONObjectCallback() {
+                            @Override
+                            public void onCompleted(JSONObject user,GraphResponse graphResponse){
+                                facebookId = user.optString("id");
+                                username = user.optString("name");
+                                email = user.optString("email");
+                            }
+                        }).executeAsync();
+
+                        try{
+                            request.get();
+                        }
+                        catch (InterruptedException ex){
+                            Log.d("EX","Interrupted");
+                        }
+                        catch (ExecutionException ex){
+                            Log.d("EX2","Execute exception");
+                        }
+
+                        String message = "LOGIN OK " + facebookId + username + email;
                         textView.setText(message, TextView.BufferType.NORMAL);
                     }
 
                     @Override
                     public void onCancel() {
                         TextView textView = (TextView) activity.findViewById(R.id.error_messages);
-                        String message = "LOGIN NOT OK";
+                        String message = "LOGIN Cancelled";
                         textView.setText(message, TextView.BufferType.NORMAL);
                     }
 
                     @Override
                     public void onError(FacebookException exception) {
                         TextView textView = (TextView) activity.findViewById(R.id.error_messages);
-                        String message = "LOGIN ERROR";
+                        String message = "LOGIN error";
                         textView.setText(message, TextView.BufferType.NORMAL);
                     }
                 });
