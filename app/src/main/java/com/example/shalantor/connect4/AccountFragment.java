67fe718e8baf255a0be309f3e62f118872c75a68
shalantor <gkaraolanis@uth.gr>
2016-11-16 19:57:00 +0200

    Login with facebook successful

--- app/src/main/java/com/example/shalantor/connect4/AccountFragment.java
+++ app/src/main/java/com/example/shalantor/connect4/AccountFragment.java
@@ -12,13 +12,20 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+
+import java.util.Arrays;
 
 public class AccountFragment extends Fragment{
 
     public static final int SCREEN_TO_TEXT_SIZE_RATIO = 20;
     public static final String USER_TYPE = "USER_TYPE";
+    private CallbackManager callbackManager;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -52,6 +59,10 @@ public class AccountFragment extends Fragment{
         address.setTextSize(displayHeight / SCREEN_TO_TEXT_SIZE_RATIO);
         address.setSelected(false);
         address.clearFocus();
+
+        /*Set permissions of login button for facebook*/
+        fbButton.setReadPermissions(Arrays.asList("public_profile","email"));
+        fbButton.setFragment(this);
 
     }
 
