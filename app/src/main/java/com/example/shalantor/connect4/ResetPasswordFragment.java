38abe241ee17ca7ed8d18e31945f461b97f12789
shalantor <gkaraolanis@uth.gr>
2016-11-19 15:11:23 +0200

    Add method to adjust components size in reset fragment

--- app/src/main/java/com/example/shalantor/connect4/ResetPasswordFragment.java
+++ app/src/main/java/com/example/shalantor/connect4/ResetPasswordFragment.java
@@ -3,15 +3,20 @@ package com.example.shalantor.connect4;
 
 import android.app.Activity;
 import android.content.pm.ActivityInfo;
+import android.graphics.Point;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class ResetPasswordFragment extends Fragment{
 
     private Activity activity;
+    public static final int SCREEN_TO_TEXT_SIZE_RATIO = 20;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -24,6 +29,24 @@ public class ResetPasswordFragment extends Fragment{
 
     /*Adjust size of components*/
     public void adjustButtons(){
+
+        /*Get references*/
+        EditText emailPrompt = (EditText) activity.findViewById(R.id.reset_mail);
+        EditText resetCodePrompt = (EditText) activity.findViewById(R.id.reset_code);
+        Button sendCode = (Button) activity.findViewById(R.id.send_code);
+        Button submitCode = (Button) activity.findViewById(R.id.submit_code);
+
+        /*Get screen dimensions*/
+        Display display = activity.getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        int displayHeight = size.y;
+
+        /*Change size of embedded text*/
+        emailPrompt.setTextSize(displayHeight / SCREEN_TO_TEXT_SIZE_RATIO);
+        resetCodePrompt.setTextSize(displayHeight / SCREEN_TO_TEXT_SIZE_RATIO);
+        sendCode.setTextSize(displayHeight / SCREEN_TO_TEXT_SIZE_RATIO);
+        submitCode.setTextSize(displayHeight / SCREEN_TO_TEXT_SIZE_RATIO);
 
     }
 
