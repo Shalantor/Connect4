7ffba241459fc6694b5dd188acdc27e5d9696937
shalantor <gkaraolanis@uth.gr>
2016-12-11 18:09:36 +0200

    Code clean up and comments

--- server/gameplayThread.py
+++ server/gameplayThread.py
@@ -24,21 +24,29 @@ def gameThread(queueToMatchMaking,queueToDatabase,exitQueue,queueForUserThread):
     #game loop
     while True:
         #read from queue first to check for new matches
+        #only read a certain amount of times, to not stall
+        #players currently in game for too long
         foundCounter = 0
         while foundCounter < MAX_FINDS:
             try:
                 newMatch = queueToMatchMaking.get(False)
+
                 #found new pair
                 foundCounter += 1
+
                 #now send singal to threads
                 firstPlayer = newMatch.get('players')[0]
                 secondPlayer = newMatch.get('players')[1]
                 firstSocket = firstPlayer.get('socket')
                 secondSocket = secondPlayer.get('socket')
+
+                #Check whose turn it is
                 turn = newMatch.get('turn')
+
                 #Assign chip number for each player
                 firstPlayer['chip'] = 1
                 secondPlayer['chip'] = 2
+
                 #Create a grid
                 newMatch['grid'] = [[0,0,0,0,0,0,0],
                                     [0,0,0,0,0,0,0],
@@ -46,8 +54,10 @@ def gameThread(queueToMatchMaking,queueToDatabase,exitQueue,queueForUserThread):
                                     [0,0,0,0,0,0,0],
                                     [0,0,0,0,0,0,0],
                                     [0,0,0,0,0,0,0]]
+
                 #Time stamp because each player has limited time for a move
                 newMatch['time'] = time.time()
+
                 #Set player turns
                 if turn == 0:
                     turn1 = '1'
@@ -55,15 +65,17 @@ def gameThread(queueToMatchMaking,queueToDatabase,exitQueue,queueForUserThread):
                 else:
                     turn1 = '0'
                     turn2 = '1'
+
                 #Send info to players
                 firstSocket.send('0 ' + turn1 + ' ' + secondPlayer.get('name') + ' \n')
                 secondSocket.send('0 ' + turn2 + ' ' + firstPlayer.get('name') + ' \n')
                 matchList.append(newMatch)
                 print '\n---- GAMEPLAY THREAD ----\n'
             except Queue.Empty:
+                #If Queue is empty, break the loop and go to active matches
                 break
 
-        #Check for exit signal
+        #Check for exit signal, to eventually stop running
         try:
             exit = exitQueue.get(False)
             break
@@ -72,13 +84,18 @@ def gameThread(queueToMatchMaking,queueToDatabase,exitQueue,queueForUserThread):
 
         #Update matches
         for match in matchList[:]:
+            #See whose turn it is
             turn = match.get('turn')
+
+            #Get socket to read the move from
             readSocket = match.get('players')[turn].get('socket')
             readSocket.settimeout(0.1)
+
             try:
                 move = readSocket.recv(512)
                 if len(move) == 0:
-                    #player lost because no move
+                    #player lost because he disconnected/terminated the app
+                    #Send winning and losing messages to both players
                     readSocket.send('2 0 0 \n')
                     loser = match.get('players')[turn]
                     turn = (turn + 1) % 2
@@ -93,18 +110,22 @@ def gameThread(queueToMatchMaking,queueToDatabase,exitQueue,queueForUserThread):
                     startNewUserThreads(queueToDatabase,queueForUserThread,match)
                     matchList.remove(match)
                 else:
+                    #Got a valid move from player
                     move = int(move)
                     print 'GAME THREAD : GOT MOVE %d ' % move
 
                 #Make move and check for win
                 result =  makeMove(match.get('grid'),move,match.get('players')[turn].get('chip'))
 
-                #Invalid move
+                #Invalid move, like out of boudns for example, or a full column
                 if not result:
                     readSocket.send('1 1 0 \n')
                 #Valid move
                 else:
+                    #Check for win
                     win = hasWon(newMatch['grid'],match.get('players')[turn].get('chip'))
+
+                    #Set states to send, depending on result of move
                     if win:
                         state1 = '1'
                         state2 = '2'
@@ -115,26 +136,36 @@ def gameThread(queueToMatchMaking,queueToDatabase,exitQueue,queueForUserThread):
                         state1 = state2 = '3'
                     else:
                         state1 = state2 = '0'
+
+                    #Now send them to players
                     readSocket.send('1 0 ' + state1 + ' \n')
+
+                    #Update time of last move
                     match['time'] = time.time()
                     match['turn'] = (match['turn'] + 1 ) % 2
                     turn = match['turn']
                     sendSocket = match.get('players')[turn].get('socket')
+
                     print 'Send data to player %s ' % match.get('players')[turn].get('name')
+
                     dataToSend = '1 ' + str(move) + ' ' + state2 + ' \n'
                     sendSocket.send(dataToSend)
+
                     #Remove from matchlist if game is over
+                    #It doesnt matter which state to check for a tie ,
+                    #because they are the same, so I chose state1
                     if win or state1 == '3':
                         #now start new threads for players
                         startNewUserThreads(queueToDatabase,queueForUserThread,match)
                         matchlist.remove(match)
 
-
             except socket.timeout:
+                #On time out, check if playe has been idle for too long
                 startTime = match['time']
                 if time.time() - startTime > MAX_WAIT:
 
-                    #player lost because no move
+                    #player lost because he didnt make a move
+                    #Send info to players
                     readSocket.send('2 0 0 \n')
                     loser = match.get('players')[turn]
                     turn = (turn + 1) % 2
@@ -149,20 +180,31 @@ def gameThread(queueToMatchMaking,queueToDatabase,exitQueue,queueForUserThread):
                     startNewUserThreads(queueToDatabase,queueForUserThread,match)
                     matchList.remove(match)
 
+
+#This function starts 2 new user threads , so that the players
+#can continue with a new match, without having to login again
 def startNewUserThreads(queueToDatabase,queueToMatchMaking,match):
 
     for i in range(0,2):
+
+        #Set default value to variables for user thread
         playerType = None
         Id = None
         userName = None
         userEmail = None
+
+        #Get player, his socket and his type
         player = match.get('players')[i]
         firstSocket = player.get('socket')
         playerType = player.get('type')
+
+        #Set his info accordingly
         if playerType == '0':
             userName = player.get('name')
             userEmail = player.get('email')
         else:
             Id = player.get('id')
+
+        #Start new thread
         uThread = Thread(target=userThread,args=(firstSocket,queueToDatabase,queueToMatchMaking,playerType,Id,userName,userEmail))
         uThread.start()
